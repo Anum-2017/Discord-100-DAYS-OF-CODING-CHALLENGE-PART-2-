c72226d250ffa901e59da38db6a0c09f69d96b8a
Anum Kamal <87061778+Anum-2017@users.noreply.github.com>
2024-05-14 14:16:10 +0500

    Add files via upload

---
+++ Day54/Article2_Variable/var_task.ts
@@ -0,0 +1,11 @@
+//Working with variables
+
+let admin:string; // Declare variable
+
+let Name:string; // Declare variable
+
+Name = 'john'; // assign value to Name variable
+
+admin = Name // Name variable value assign to admin
+
+alert(admin); // Show the value of admin using alert
