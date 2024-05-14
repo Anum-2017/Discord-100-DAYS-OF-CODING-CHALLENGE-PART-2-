bf4f958ed614ade2905fcc2951cf34931adafeda
Anum Kamal <87061778+Anum-2017@users.noreply.github.com>
2024-05-14 16:17:53 +0500

    Add files via upload

---
+++ Day55/article1_datatypes/datatypes.js
@@ -0,0 +1,12 @@
+// What is the output of the script?
+
+let name = "Anum";
+
+// the expression is a number 1
+alert( `hello ${1}` ); 
+
+// the expression is a string "name"
+alert( `hello ${"name"}` ); 
+
+// the expression is a variable, embed it
+alert( `hello ${name}` );
