3cac67bb58024acb1cf825fe94708597eda3caf9
Anum Kamal <87061778+Anum-2017@users.noreply.github.com>
2024-05-27 13:58:50 +0500

    Delete Day54 directory

--- Day54/Article2_Variable/var_task.js
+++
@@ -1,6 +0,0 @@
-//Working with variables
-var admin; // Declare variable
-var Name; // Declare variable
-Name = 'john'; // assign value to Name variable
-admin = Name; // Name variable value assign to admin
-alert(admin); // Show the value of admin using alert
