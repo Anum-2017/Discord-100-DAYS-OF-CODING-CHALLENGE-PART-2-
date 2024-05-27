29e906fa043e44fea3d7ccbf8d45229949499889
Anum Kamal <87061778+Anum-2017@users.noreply.github.com>
2024-05-27 14:38:43 +0500

    Add files via upload

---
+++ Day54/var.js
@@ -0,0 +1,14 @@
+// Task 1
+let admin;
+let name;
+name = "John";
+admin = name;
+alert(admin); // John
+
+// Task 2
+let ourPlanet = "Earth";
+let currentVisitor = "Anum";
+
+// Task 3
+const BIRTHDAY = "22-10-1995";
+const age = 29;
